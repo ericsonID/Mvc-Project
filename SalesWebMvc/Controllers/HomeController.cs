b5b6e9a2929dcf9d2debc473c47d53b41f3dc299
Ericson Cordeiro de Lima <ericson.id@gmail.com>
2021-04-18 11:18:19 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from C# Course";
+            ViewData["Student"] = "Ericson Cordeiro de Lima";
 
             return View();
         }
