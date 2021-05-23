d6a0743a420b613f247a4b313dfbae90dd9e71f7
Ericson Cordeiro de Lima <ericson.id@gmail.com>
2021-05-23 15:13:40 -0300

    Simple Create Form

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMvc.Services;
-
+using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Controllers
 {
@@ -22,5 +22,18 @@ namespace SalesWebMvc.Controllers
             return View(list);
             
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+
+        }
+        
     }
 }
--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -17,5 +17,10 @@ namespace SalesWebMvc.Services
         {
             return _context.Seller.ToList();
         }
+        public void Insert (Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
