392e0bfd566188d55dbdf34f56459385a8c46276
Ericson Cordeiro de Lima <ericson.id@gmail.com>
2021-07-31 18:01:01 -0300

    Last Push

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -47,6 +47,7 @@ namespace SalesWebMvc
             services.AddScoped<SeedingService>();
             services.AddScoped<SellerService>();
             services.AddScoped<DepartmentService>();
+            services.AddScoped<SalesRecordService>();
 
         }
 
